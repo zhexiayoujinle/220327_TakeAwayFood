91b0af65cb04ecd8afe433ef764ca0415481ee57
Zj <1360367781@qq.com>
2022-04-01 22:42:33 +0800

    v1.3

--- src/store/actions.js
+++ src/store/actions.js
@@ -92,12 +92,13 @@ export default {
         }
     },
     // 异步获取商家商品列表
-    async getShopGoods({ commit }) {
+    async getShopGoods({ commit }, callback) {
         const result = await reqShopGoods()
         if (result.code === 0) {
             const goods = result.data
             commit(RECEIVE_GOODS, { goods })
             // 如果组件中传递了接收消息的回调函数, 数据更新后, 调用回调通知调用的组件
+            callback && callback()
         }
     },
 }
